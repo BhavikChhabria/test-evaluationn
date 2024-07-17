154f1798e3c0cf979ba6c8899e197debc2a9e5cf
bha1212002 <bhavikchhabria@gmaial.com>
2024-07-17 08:58:05 +0530

    Successfully Added

---
+++ shell_1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+read -p "Enter the time (HH:MM): " input_time
+
+if [[ "$input_time" > "00:00" && "$input_time" < "06:01" ]]; then
+  echo "Early Morning"
+elif [[ "$input_time" > "06:00" && "$input_time" < "12:01" ]]; then
+  echo "Morning"
+elif [[ "$input_time" > "12:00" && "$input_time" < "18:01" ]]; then
+  echo "Afternoon"
+elif [[ "$input_time" > "18:00" && "$input_time" < "23:59" ]]; then
+  echo "Late Night"
+else
+  echo "Invalid time input."
+fi
+
