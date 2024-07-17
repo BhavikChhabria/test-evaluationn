a182252dbaf2900aff4f9e58ee6fc81cd31005c5
bha1212002 <bhavikchhabria@gmaial.com>
2024-07-17 15:00:00 +0530

    successfully updated

--- shell_t1.sh
+++ shell_t1.sh
@@ -8,7 +8,7 @@ elif [[ "$input_time" > "06:00" && "$input_time" < "12:01" ]]; then
   echo "Morning"
 elif [[ "$input_time" > "12:00" && "$input_time" < "18:01" ]]; then
   echo "Afternoon"
-elif [[ "$input_time" > "18:00" && "$input_time" < "23:59" ]]; then
+elif [[ "$input_time" > "18:00" && "$input_time" < "24:01" ]]; then
   echo "Late Night"
 else
   echo "Invalid time input."
